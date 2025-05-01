385e2e6226bf84d434e0770770128a8b1862c156
abhaynaryal <naryalabhay@gmail.com>
2025-05-01 18:30:03 +0530

    first commit

--- Backend/server.js
+++ Backend/server.js
@@ -1,23 +1,25 @@
-
 const express = require("express");
-const cors = require("cors")
+const cors = require("cors");
 const connectDB = require("./config/db");
 require("dotenv").config();
 
 const app = express();
 
-// ✅ CORS Setup (Allow requests from frontend)
-app.use(cors());
 
-// JSON middleware
+app.use(
+  cors({
+    origin: "*", 
+    methods: ["GET", "POST", "PUT", "DELETE"], 
+    // allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true, 
+  })
+);
+
 app.use(express.json());
 
-// Connect to MongoDB
 connectDB();
 
-// Routes
 app.use("/api/auth", require("./routes/auth"));
 
-// Start server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
